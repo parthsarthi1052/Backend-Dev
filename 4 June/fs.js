b395577b8d8033bcbeff78747d17b5f4fbdc6282
Parth Sarthi <parthsarthi@Parths-MacBook-Air.local>
2026-06-08 21:58:03 +0530

    Day-3

---
+++ 4 June/fs.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+// console.log(fs);
+
+// sync->Blocing Operations
+// let result=fs.readFileSync('file.txt',"utf8");
+// console.log(result);
+
+// Async->Non-Blocking Operation
+
+// fs.readFile("file.txt","utf8",(error,data)=>{
+//     if (error) {
+//         throw error;
+//     }
+//     console.log(data);
+    
+// })
+// console.log("yogesh bauna");
